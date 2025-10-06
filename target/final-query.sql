4564524980c6ce8b83e3b3451c722c8e4d8f595f
ayush26009 <k.ayush@unque.me>
2025-10-06 17:32:13 +0530

    JAR Added

---
+++ target/final-query.sql
@@ -0,0 +1,1 @@
+SELECT e.emp_id, e.first_name, e.last_name, d.department_name, (SELECT COUNT(*) FROM employee e2 WHERE e2.department = e.department AND e2.dob > e.dob) AS younger_employees_count FROM employee e JOIN department d ON d.department_id = e.department ORDER BY e.emp_id DESC;
